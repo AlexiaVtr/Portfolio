1b09cd6883b0905b8bcda7913137fc90ea9944c6
Alex <alexiavetere@gmail.com>
2022-03-13 14:42:30 -0300

    1.1.0

---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+    <img src={images.log} alt='logo'/>
+    </div>
+)}
+
+export default Navbar
