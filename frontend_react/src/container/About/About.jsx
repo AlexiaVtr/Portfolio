73f6d4e8cf08ead948606d1d2f807cccc6654fff
Alex <alexiavetere@gmail.com>
2022-03-14 07:42:15 -0300

    1.1.1

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './About.scss';
 
+const abouts= [
+{tittle: 'Web Development', description: 'I am a good developer'}
+];
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>
+      I know that
+      <span>Good Desing</span>
+      <br />
+      means
+      <span>Good Business</span>
+    </h2>
+    <div className='app_profiles'>
+
+    </div>
+    </>
   )
 }
 
